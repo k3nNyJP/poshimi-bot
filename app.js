5baf3341160819cae5bc5d9c5e891153ed3d2d8d
k3nNyJP <k3nny.51rcy@gmail.com>
2020-07-12 03:24:23 +0900

    Initial version

---
+++ app.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+const { DISCORD_POSHIMI_TOKEN } = process.env;
+const reactionWordList = ["ぽしみ", "ぽやしみ", "ぽやすみ"];
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("message", async (msg) => {
+  if (
+    msg.author.tag !== client.user.tag &&
+    reactionWordList.some((word) => msg.content.includes(word))
+  ) {
+    msg.channel.send(Math.random() * 100 > 1 ? "ぽしみ〜" : "ぽやしみ〜");
+  }
+});
+
+client.login(DISCORD_POSHIMI_TOKEN);
