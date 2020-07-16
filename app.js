000899194dc7eb8a2039db3175ad92507619ef4b
k3nNyJP <k3nny.51rcy@gmail.com>
2020-07-17 02:59:51 +0900

    add '?uragiri' reaction

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const client = new Discord.Client();
 
 const { DISCORD_TOKEN } = process.env;
 const reactionWordList = ["ぽしみ", "ぽやしみ", "ぽやすみ"];
+const uragiriCommandTrigger = "?uragiri";
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -28,6 +29,9 @@ client.on("message", async (msg) => {
         msg.channel.send("ぽしみ〜");
         return;
     }
+  } else if (!msg.author.bot && msg.content === uragiriCommandTrigger) {
+    const rng = Math.random() * 100;
+    msg.channel.send(`ひどいよ ${rng <= 1 ? `:pleading_face:` : `:sob:`}`);
   }
 });
 
